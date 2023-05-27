5796ee963e38824670f28549487a0d841621df71
Prince Neres <prince84224960@gmail.com>
2023-05-27 00:36:55 -0300

    Login and Register template DRF with JWT

---
+++ authentication/renderers.py
@@ -0,0 +1,16 @@
+from core.renderers import ConduitJSONRenderer
+
+
+class UserJSONRenderer(ConduitJSONRenderer):
+    charset = 'utf-8'
+    object_label = 'user'
+    pagination_object_label = 'users'
+    pagination_count_label = 'usersCount'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        token = data.get('token', None)
+
+        if token is not None and isinstance(token, bytes):
+            data['token'] = token.decode('utf-8')
+
+        return super(UserJSONRenderer, self).render(data)
